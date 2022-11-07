d04292f00501d7889eb9dc335bc5fd3804561177
VardanAntanosyan06 <vantanosyan99@gmail.com>
2022-11-07 15:48:04 +0400

    commit for only upload

--- src/js/Login.js
+++ src/js/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
         </div>
         <div class="form-field">
          <Link to="admin" >
-         <button class="btn">
+         <button class="btn" type="submit">
             Log in
           </button>
          </Link>
@@ -25,30 +25,7 @@ const Login = () => {
         </div>
       </form>
     </div>
-  );
-  fetch("https://jsonplaceholder.typicode.com/posts", {
+  )}
 
-    // Adding method type
-    method: "POST",
-     
-    // Adding body or contents to send
-    body: JSON.stringify({
-        title: "foo",
-        body: "bar",
-        userId: 1
-    }),
-     
-    // Adding headers to the request
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-})
- 
-// Converting to JSON
-.then(response => response.json())
- 
-// Displaying results to console
-.then(json => console.log(json));
-};
-
+  
 export default Login;
